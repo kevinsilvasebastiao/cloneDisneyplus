3dbc897f2f7f5f97ea8b6ed89ffc92de0d3424fd
kevin <kevinsilva41@gmail.com>
2024-02-02 09:56:43 -0300

    ajustes no mobile

--- src/main.js
+++ src/main.js
@@ -1,7 +1,25 @@
 //função responsavel por fazer a incrementção para mudaça na pagina entre as opções de filmes
 document .addEventListener('DOMContentLoaded', function() {
+
     //puchando os valore no html com nome de data-tab-button
     const buttons = document.querySelectorAll('[data-tab-button]');
+
+    //puchando os valore no html com nome de data-faq-quastion
+    const questions = document.querySelectorAll('[data-faq-quastion]');
+
+    const heroSection = document.querySelector('.hero');
+    const alturaHero = heroSection.clientHeight;
+
+    window.addEventListener('scroll', function () {
+        const posisaoAtual = window.scrollY;
+
+        if (posisaoAtual < alturaHero) {
+            ocultaElementosdoHeader();
+        }else {
+            exibeElementosdoHeader();
+        }
+    })
+
 //incrementeção do codigo responsavel por fazer a mudança do catalogo quando ouver o evento de click puxando tbm os id no html passando o valor 
 //adicionando o "is active" para qual devera ser exibida
     for (let i = 0; i < buttons.length; i++) {
@@ -13,6 +31,27 @@ document .addEventListener('DOMContentLoaded', function() {
             removeBotaoAtivo();
             botao.target.classList.add('shows__tabs__button--is-active');
         })
+    }
+    //para adicionar o feito de abrir e fechar as perguntas frequentes 
+    for (let i = 0; i < questions.length; i++) {
+        questions[i].addEventListener('click' , abreOuFechaResposta);
+    }
+
+    function ocultaElementosdoHeader() {
+        const header  = document.querySelector('header');
+        header.classList.add('header--is-hidden');
+    }
+
+    function exibeElementosdoHeader() {
+        const header  = document.querySelector('header');
+        header.classList.remove('header--is-hidden');
+    }
+
+    function abreOuFechaResposta(elemento) {
+        const classe = 'faq__questions__item--is-open';
+        const elementoPai = elemento.target.parentNode;
+
+        elementoPai.classList.toggle(classe);
     }
 })
 //função responsavel por mudar qual botao esta ativo no momento para visualização do usuario final
